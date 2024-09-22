f7b7049ee5a59dbd01d9fff2f166283edea48d35
athul <athuli057@gmail.com>
2024-09-22 09:26:26 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-
 import './App.css';
 import Body from './Components/Body';
 import Header from './Components/Header';
 import store from './Components/utils/store';
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MainContainer from './Components/MainContainer';
+import WatchPage from './Components/WatchPage';
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Body />,
+    children: [
+      {
+        path: '', // Use empty path for MainContainer under '/'
+        element: <MainContainer />
+      },
+      {
+        path: 'watch',
+        element: <WatchPage />
+      }
+    ]
+  },
+]);
 
 function App() {
   return (
     <Provider store={store}>
       <div className="">
-        {/* 
-      Head
-      Body
-        Side Bar
-          Menu Items
-        MainContainer
-          ButtonList
-          Video Container
-            Video Cards
-      */}
-
         <Header />
-        <Body />
+        <RouterProvider router={appRouter} />
       </div>
-    </Provider >
+    </Provider>
   );
 }
 
--- src/Components/Body.js
+++ src/Components/Body.js
@@ -2,12 +2,14 @@ import React from 'react'
 import SideBar from './SideBar'
 import MainContainer from './MainContainer'
 import { useSelector } from 'react-redux'
+import { Outlet } from 'react-router-dom'
 
 const Body = () => {
  return (
   <div className='flex'>
    <SideBar />
-   <MainContainer />
+   <Outlet />
+
   </div>
  )
 }
